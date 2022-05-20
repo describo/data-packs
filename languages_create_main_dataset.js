960562092b8a058e46fa1d132088b778670f0203
Supertyp <barth.wolf@gmail.com>
2022-05-20 10:30:13 +1000

    Deleted unused files

--- languages_create_main_dataset.js
+++
@@ -1,8 +0,0 @@
-const chirillaMapping = require("./Chirila-language-data/chirilla_mapping.json");
-var data_glottolog = require("./Glottolog-language-data/glottolog-language-data-pack.json")
-const fetch = require("cross-fetch");
-const { writeJson } = require("fs-extra");
-
-
-console.log(data_glottolog)
-
