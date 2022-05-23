e5add3f096e7213d72ae94060f48833d3610b506
Supertyp <barth.wolf@gmail.com>
2022-05-23 11:02:14 +1000

    adds lookup_datapack.js

---
+++ lookup_datapack.js
@@ -0,0 +1,52 @@
+
+//  * packName: name of the data pack: e.g. countries, languages, camera's etc
+//  * find: query string - lookup in @id and name fields
+  
+  
+
+//export function lookup({ packName, find }) {
+const languages = require("./languages-main-data-pack.json")
+const countries = require("./country-main-data-pack.json")
+
+export function lookup(packName, find, quantity) {
+	// find substring in items in datapack
+
+	var matchedItems = []
+	
+	function itemFound(item) {
+		if (!(item in matchedItems)) {
+		    matchedItems.push(item);
+		}
+	}
+	
+	for (let i = 0; i < packName.length; i++) {
+		
+	    var item = packName[i];
+		
+		// look in names
+		if (item["name"].startsWith(find)) {
+			itemFound(item);
+			
+		// look in IDs
+		} else if (item["@id"].includes(find)) {
+			itemFound(item);
+		
+		// look in alternate Names
+		} else if (("alternateName" in item) && 
+		    (typeof item["alternateName"] !== 'undefined') && 
+			(item["alternateName"].includes(find))) {
+				itemFound(item);		
+		}
+ 
+		if (matchedItems.length >= quantity) {
+			return matchedItems;	
+	    }  			
+	}
+	
+	return foundMatch
+};
+
+var matchedLanguages = lookup(languages, "A", 10)
+var matchedCountries = lookup(countries, "An", 2)
+//console.log(matchedLanguages)
+//console.log(matchedCountries)
