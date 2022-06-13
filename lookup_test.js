1599d5e13db888e2e6d84a82d8a18c1aa6bc0602
Supertyp <barth.wolf@gmail.com>
2022-06-13 11:01:01 -0500

    renamed lookup.js

--- lookup_test.js
+++ lookup_test.js
@@ -1,14 +1,5 @@
+const lookup_chained = require("./lookup.js")
 
-const lookup = require("./lookup_datapack.js")
-const lookup_chained = require("./lookup_chained.js")
-
-
-
-//let promise = Promise.resolve(lookup({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}}))
-//let promise = Promise.resolve(lookup({packName: "Languages", find: "atukar", filter: {"source": "Glottolog"}}))
-//let promise = Promise.resolve(lookup({ packName: "Glottolog", find: "atukar", filter: {"source": "Glottolog", "@type": "Language"}}))
-//let promise = Promise.resolve(lookup({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}}))
- 
 
 //let promise = Promise.resolve(lookup_chained({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}}))
 //let promise = Promise.resolve(lookup_chained({packName: "Languages", find: "atukar", filter: {"source": "Glottolog"}}))
