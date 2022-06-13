698c16a6b35dc9179cac7c6d794186e10396649c
Supertyp <barth.wolf@gmail.com>
2022-06-13 11:12:16 -0500

    clean lookup_test.js

--- lookup_test.js
+++ lookup_test.js
@@ -1,11 +1,11 @@
-const lookup_chained = require("./lookup.js")
+const lookup = require("./lookup.js")
 
 
-//let promise = Promise.resolve(lookup_chained({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}}))
-//let promise = Promise.resolve(lookup_chained({packName: "Languages", find: "atukar", filter: {"source": "Glottolog"}}))
-//let promise = Promise.resolve(lookup_chained({ packName: "Glottolog", find: "atuk", filter: {"source": "Glottolog"}}))
-//let promise = Promise.resolve(lookup_chained({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}})) 
-let promise = Promise.resolve(lookup_chained({ packName: "Languages", find: "li", filter: {"source": "Glottolog", "@type": "Language"}}))
+//let promise = Promise.resolve(lookup({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}}))
+//let promise = Promise.resolve(lookup({packName: "Languages", find: "atukar", filter: {"source": "Glottolog"}}))
+//let promise = Promise.resolve(lookup({ packName: "Glottolog", find: "atuk", filter: {"source": "Glottolog"}}))
+//let promise = Promise.resolve(lookup({ packName: "Languages", find: "Matukar", filter: {"source": "Glottolog", "@type": "Language"}})) 
+let promise = Promise.resolve(lookup({ packName: "Languages", find: "li", filter: {"source": "Glottolog", "@type": "Language"}}))
 
 
 promise.then(function(val) {
