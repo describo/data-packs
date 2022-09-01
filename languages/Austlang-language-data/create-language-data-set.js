074b59e07682110344b52970283cefe94e3588e4
Marco La Rosa <m@lr.id.au>
2022-09-01 14:34:57 +1000

    Squashed commit of the following:
    commit f8fcea6af58c2eb2cd15873de634486d27ca37b1
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 14:29:57 2022 +1000
    
        add documentation
    
    commit e82246beff31f70ace3f18cd74d5c156c530bb3b
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 14:29:38 2022 +1000
    
        add jest and jsdoc config
    
    commit 1beed8a03442e3bb5331bfc0a045e4b10d990db4
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 14:29:28 2022 +1000
    
        cleanup
    
    commit ccfcef7201a5839279d4d17529df77b4fce956a8
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 14:29:14 2022 +1000
    
        change name to arkisto/describo-data-packs
    
    commit 7189c47ed85039b85350badd42538b9b6929a262
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 14:20:14 2022 +1000
    
        documentation
    
    commit b35854785dd443acb48c3861af96e15a73fe37ff
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 14:19:59 2022 +1000
    
        add jest and jsdoc packages
    
    commit aa4bd2413c5a1a703cf542bbf5b82f61f90b700f
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 13:46:55 2022 +1000
    
        refactor; implement datapack class for lookups
    
    commit c6ef37f9e59128f1e25194870485000372334dea
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Thu Sep 1 13:46:37 2022 +1000
    
        formatting; write out json files without spaces
    
    commit 0c3a58c2697c506b2b869242adbb2fe048f4b3db
    Author: Marco La Rosa <m@lr.id.au>
    Date:   Wed Aug 31 13:21:15 2022 +1000
    
        update dependencies

--- languages/Austlang-language-data/create-language-data-set.js
+++ languages/Austlang-language-data/create-language-data-set.js
@@ -71,6 +71,7 @@ function CSVtoArray(strData, strDelimiter) {
         arrData[arrData.length - 1].push(strMatchedValue);
     }
 
+<<<<<<< HEAD
     // Now that we have our value string, let's add
     // it to the data array.
     arrData[arrData.length - 1].push(strMatchedValue);
@@ -79,6 +80,10 @@ function CSVtoArray(strData, strDelimiter) {
   // Return the parsed data.
 
   return (arrData);
+=======
+    // Return the parsed data.
+    return arrData;
+>>>>>>> update
 }
 
 (async () => {
