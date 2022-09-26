1af3a985f9cdacbc73dbb15e399caacc63f0b7d4
Marco La Rosa <m@lr.id.au>
2022-09-27 09:11:30 +1000

    fix documentation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 /**
+ * @global
+ *
+ * @description The data packs that are available in this repository.
+ *
  * @property {object}  packs - the available data packs
  * @property {String}  packs.Austlang - The Austlang language data pack
  * @property {String}  packs.Glottolog - The Glottolog language data pack
- * @property {String}  packs.Ethnologue - The Ethnologue language data pack
  * @property {String}  packs.Country - The Countries data pack
- * @property {String}  packs.Languages - All languages data pack
+ * @property {String}  packs.Languages - All languages data pack = ["Austlang", "Glottolog" ]
  */
 const datapacks = {
     Austlang: "languages/austlang-language-data-pack.json",
