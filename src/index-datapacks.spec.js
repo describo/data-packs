a68e20e5d2f36cb8a38f5572f3e7828a488b8eae
Marco La Rosa <m@lr.id.au>
2022-11-22 14:10:47 +1100

    remove only

--- src/index-datapacks.spec.js
+++ src/index-datapacks.spec.js
@@ -1,6 +1,6 @@
 const { IndexDataPacks } = require("./index-datapacks.js");
 
-describe.only(`Test indexing the data packs`, () => {
+describe(`Test indexing the data packs`, () => {
     it(`should be able to index all of the data packs`, async () => {
         const index = new IndexDataPacks({ elasticUrl: "http://localhost:9200" });
         await index.load();
