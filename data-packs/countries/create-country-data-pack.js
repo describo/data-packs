0d8abdfda33d1af110c6faff1d0f4362ff421b05
Marco La Rosa <m@lr.id.au>
2023-10-16 12:40:37 +1100

    revise to not require ethnologue

--- data-packs/countries/create-country-data-pack.js
+++ data-packs/countries/create-country-data-pack.js
@@ -1,37 +1,25 @@
-const countryCodesTab = "https://www.ethnologue.com/sites/default/files/CountryCodes.tab";
+// const countryCodesTab = "https://www.ethnologue.com/sites/default/files/CountryCodes.tab";
+const countryCodesJson = "https://datahub.io/core/country-codes/r/country-codes.json";
 const countryGeoJSON = "https://datahub.io/core/geo-countries/r/countries.geojson";
 const languagePack = "./iso-639-6-country-data-pack.json";
-const countryISOMapping = require("./mapping-iso3-iso2.json"); // from https://github.com/vtex/country-iso-3-to-2
 const fetch = require("cross-fetch");
 const { writeJson } = require("fs-extra");
 const { groupBy } = require("lodash");
-const iso2to3Mapping = flipIsoMapping();
 
 main();
 async function main() {
-    let countryCodes = await get({ url: countryCodesTab });
-    const lines = countryCodes.split("\n").map((l) => l.trim());
+    const countries = JSON.parse(await get({ url: countryCodesJson }));
 
     let countryData = [];
-    for (let line of lines.slice(1, lines.length)) {
-        let components, code, country, name;
-        try {
-            let [code, name, region] = line.split("\t");
-            if (name && code) {
-                // console.log(code, country, name);
-                countryData.push({
-                    "@id": `https://www.ethnologue.com/country/${code}`,
-                    "@type": "Country",
-                    name,
-                    isoA2: code,
-                    isoA3: iso2to3Mapping[code],
-                });
-            }
-        } catch (error) {
-            console.log(error.message, components);
-        }
+    for (let country of countries) {
+        countryData.push({
+            "@id": `https://www.ethnologue.com/country/${country["ISO3166-1-Alpha-2"]}`,
+            "@type": "Country",
+            name: country.official_name_en,
+            isoA2: country["ISO3166-1-Alpha-2"],
+            isoA3: country["ISO3166-1-Alpha-3"],
+        });
     }
-
     let countryGeoJSONData = await get({ url: countryGeoJSON, as: "json" });
     countryGeoJSONGroupedByName = groupBy(countryGeoJSONData.features, (c) => c.properties.ISO_A3);
 
@@ -65,10 +53,4 @@ async function get({ url, as = "text" }) {
     } else if (as === "json") {
         return await response.json();
     }
-}
-
-function flipIsoMapping() {
-    let mapping = {};
-    Object.keys(countryISOMapping).forEach((key) => (mapping[countryISOMapping[key]] = key));
-    return mapping;
 }
