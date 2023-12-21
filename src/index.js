d0b3a6bd5f2bf2620055ea12425bf7f969adb374
Marco La Rosa <m@lr.id.au>
2023-12-21 16:31:51 +1100

    update data pack export with context information

--- src/index.js
+++ src/index.js
@@ -10,9 +10,23 @@
  * @property {String}  packs.Languages - All languages data pack = ["Austlang", "Glottolog" ]
  */
 const datapacks = {
-    Austlang: "languages/austlang-language-data-pack.json",
-    Glottolog: "languages/glottolog-language-data-pack.json",
-    Country: "countries/iso-639-6-country-data-pack.json",
+    Austlang: {
+        path: "languages/austlang-language-data-pack.json",
+        description:
+            "Information about Aboriginal and Torres Strait Islander languages which has been assembled from a number of referenced sources",
+        source: "https://collection.aiatsis.gov.au/austlang/about",
+    },
+    Glottolog: {
+        path: "languages/glottolog-language-data-pack.json",
+        description:
+            "Information about the different languages, dialects, and families of the world ",
+        source: "https://glottolog.org",
+    },
+    Country: {
+        path: "countries/iso-639-6-country-data-pack.json",
+        description: "ISO-639-6 country data",
+        source: "https://datahub.io/core/country-codes/r/country-codes.json",
+    },
     Languages: ["Austlang", "Glottolog"],
 };
 
